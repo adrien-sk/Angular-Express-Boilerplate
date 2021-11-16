b5b32a2f60daf7710ea3764e6e688cb759b44adb
Adrien_Sk <skrzypczak.adrien@gmail.com>
2021-11-16 20:02:16 +0100

    Simple Api Call Example

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,22 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+
+export interface Foo {
+  id: number;
+  username: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'Angular-Express-Boilerplate';
+  testApi!: Observable<Foo[]>;
+
+  constructor(private http: HttpClient) {
+    this.testApi = this.http.get<Foo[]>('http://localhost:8080/v1/foo');
+  }
 }
